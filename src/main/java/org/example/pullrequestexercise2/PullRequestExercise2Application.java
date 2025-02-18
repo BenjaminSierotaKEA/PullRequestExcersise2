5e75705ae161f157ec38a90953452a135456aa6c
BenjaminSierotaKEA <the.crazy.thing.is@gmail.com>
2025-02-18 11:06:04 +0100

    Initial

---
+++ src/main/java/org/example/pullrequestexercise2/PullRequestExercise2Application.java
@@ -0,0 +1,13 @@
+package org.example.pullrequestexercise2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PullRequestExercise2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PullRequestExercise2Application.class, args);
+    }
+
+}
